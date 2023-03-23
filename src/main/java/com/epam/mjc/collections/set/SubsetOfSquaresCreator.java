e146665f12fa6e3eff0dec9fef1cceb82c53e77e
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-23 14:45:33 +0300

    Update SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -5,5 +5,24 @@ import java.util.Set;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+      Set<Integer> inter = new HashSet<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if (sourceList.get(i) < 0) {
+                int a = sourceList.get(i) * -1;
+                int s = a * a;
+                if (s >= lowerBound && s <= upperBound) {
+                    inter.add(s);
+                }
+            } else {
+                int s = sourceList.get(i) * sourceList.get(i);
+                if (s >= lowerBound && s <= upperBound) {
+                    inter.add(s);
+                }
+            }
+
+        }
+
+        return inter;
     }
 }
