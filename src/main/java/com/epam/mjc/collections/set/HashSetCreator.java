67b0498d4aa73f125dc88e0bddd1687013ea6b64
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-23 14:44:45 +0300

    Update HashSetCreator.java

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,24 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+     HashSet<Integer> inter = new HashSet<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if (sourceList.get(i) % 2 == 0) {
+                inter.add(sourceList.get(i));
+                int a = sourceList.get(i) / 2;
+                inter.add(a);
+                while (a % 2 == 0) {
+                    a = a / 2;
+                    inter.add(a);
+                }
+            } else {
+                inter.add(sourceList.get(i));
+                int b = sourceList.get(i) * 2;
+                inter.add(b);
+            }
+        }
+
+        return inter;
     }
 }
